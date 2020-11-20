3512f2db065a4fc29f615d45a99250105fd3e267
lwq <15565680981@163.com>
2020-11-20 12:17:11 +0800

    l2 登录页面交互已完成

--- login/login.js
+++ login/login.js
@@ -13,32 +13,23 @@ $("#goto-login").on("click", function () {
 var form = layui.form;
 form.verify({
     // 规则名：函数，必须有return，不符合正则提醒信息
-    changdu:[/^\S{6,12}$/,'长度为6-12位，不能有空格'],
+    changdu: [/^\S{6,12}$/, '长度为6-12位，不能有空格'],
     // 规则名：[正则、不符合正则提醒信息]
-    same:function(val){
-    // 再次输入：val
-    // 第一次输入：直接获取
-    if($('.pwd').val() != val){
-        return "两次输入的密码不一致";
+    same: function (val) {
+        // 再次输入：val
+        // 第一次输入：直接获取
+        if ($('.pwd').val() != val) {
+            return "两次输入的密码不一致";
+        }
     }
-    }
-   
-    
+
+
 })
 
 // ----------------注册--------
 $('#register .layui-form').on("submit", function (e) {
     // 1.阻止默认行为
     e.preventDefault();
-   
-
-
-
-
-
-
-
-
     // 2.收集信息
     var data = $(this).serialize();
     // 3.提交信息
@@ -48,12 +39,39 @@ $('#register .layui-form').on("submit", function (e) {
         data: data,
         success: function (res) {
             layer.msg(res.message);
-            if(res.status == 0) {
+            if (res.status == 0) {
+
                 $("#register").stop().hide();
                 $("#login").stop().show();
 
                 // 清空表单 ：原生方法；重置
                 $('#register .layui-form')[0].reset();
+            }
+
+
+        }
+    });
+})
+
+// ----------------登录--------
+$('#login form').on("submit", function (e) {
+    // 1.阻止默认行为
+    e.preventDefault();
+    // 2.收集账号、密码信息
+    var data = $(this).serialize();
+    // 3.提交信息
+    $.ajax({
+        type: 'POST',
+        url: 'http://ajax.frontend.itheima.net/api/login',
+        data: data,
+        success: function (res) {
+            layer.msg(res.message);
+            if (res.status == 0) {
+
+                // 把token保存到本地存储
+                localStorage.setItem('token', res.token)
+                // 登陆成功，页面跳转到首页
+                location.href = '/index.html';
             }
         }
     });
@@ -61,5 +79,5 @@ $('#register .layui-form').on("submit", function (e) {
 
 
 
-  
+
 
