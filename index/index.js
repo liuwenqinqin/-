788f1dd5c1dfd03218358e555fc71e4167986793
lwq <15565680981@163.com>
2020-11-20 17:37:17 +0800

    l5 退出以及token的作用设置完成

--- index/index.js
+++ index/index.js
@@ -1,9 +1,15 @@
+// --------------设计的第一层意思
+if(!localStorage.getItem("token")){
+    location.href = '../login.html';
+}
+
 // -------------请求个人信息-----
 $.ajax({
     url:"http://ajax.frontend.itheima.net/my/userinfo",
     headers:{
         Authorization:localStorage.getItem("token"),
     },
+    // 请求成功后调用
     success:function(res){
         // console.log(res);
         // 名称：有昵称就昵称、不然就是用户名；
@@ -27,5 +33,36 @@ $.ajax({
                 $(".layui-nav-img").hide();
             }
         }
+    },
+    // 请求失败后调用
+    fail:function(){},
+    // 完成后，不管成功还是失败，都会执行这个函数
+    complete:function(xhr){
+        // xhr:经过JQ封装后，xhr对象
+        // 原生xhr找到返回数据，xhr.responseText；
+        // xhr.responseJSON
+
+        if(xhr.responseJSON.status == 1 || xhr.responseJSON.message == "身份认证失败！"){
+            localStorage.removeItem("token");
+            location.href = "../login.html";
+        }
+
+        
     }
+})
+
+
+// -----------------退出-----------
+// 1.点击退出
+// 2.优化，弹窗，询问是否退出？
+$("#logout").on("click", function(){
+    layer.confirm("确定退出嘛？", {
+        icon:3,
+        title:"提示退出"
+    }, function(index){
+        localStorage.removeItem("token");
+        location.href = "../login.html";
+        layer.close(index);
+    }
+    )
 })
