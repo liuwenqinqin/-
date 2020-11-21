b5e5f2add5d9ed3f6c2333a28a577174def5043b
lwq <15565680981@163.com>
2020-11-21 11:08:53 +0800

    l7 个人中心的基本资料更新功能完成

--- index/index.js
+++ index/index.js
@@ -4,37 +4,41 @@ if(!localStorage.getItem("token")){
 }
 
 // -------------请求个人信息-----
-$.ajax({
-    url:"/my/userinfo",
-    
-    // 请求成功后调用
-    success:function(res){
-        // console.log(res);
-        // 名称：有昵称就昵称、不然就是用户名；
-        if(res.status == 0 ){
-            var name = res.data.nickname || res.data.username;
-            $(".username").text(name);
-            // 头像：如果有头像数据
-            if(res.data.uaer_pic){
-                $(".layui-nav-img").show().prop("src", res.data.uaer_pic);
-                $('.avatar').hide();
-            }
-            // 测试：没有头像数据的时候
-            else{
-                // 截取name名字上第一个字符；
-                var t = name.substr(0,1);
-                // 英文字符：小写变为大写：字符串.toUpperCase()
-                t = t.toUpperCase();
+get();
 
-                // show:会让元素变为行内元素；
-                $('.avatar').show().css("display", "inline-block").text(t);
-                $(".layui-nav-img").hide();
-            }
-        }
-    },
+function get() {
+    $.ajax({
+        url:"/my/userinfo",
+        
+        // 请求成功后调用
+        success:function(res){
+            // console.log(res);
+            // 名称：有昵称就昵称、不然就是用户名；
+            if(res.status == 0 ){
+                var name = res.data.nickname || res.data.username;
+                $(".username").text(name);
+                // 头像：如果有头像数据
+                if(res.data.uaer_pic){
+                    $(".layui-nav-img").show().prop("src", res.data.uaer_pic);
+                    $('.avatar').hide();
+                }
+                // 测试：没有头像数据的时候
+                else{
+                    // 截取name名字上第一个字符；
+                    var t = name.substr(0,1);
+                    // 英文字符：小写变为大写：字符串.toUpperCase()
+                    t = t.toUpperCase();
     
-})
-
+                    // show:会让元素变为行内元素；
+                    $('.avatar').show().css("display", "inline-block").text(t);
+                    $(".layui-nav-img").hide();
+                }
+            }
+        },
+        
+    })
+    
+}
 
 // -----------------退出-----------
 // 1.点击退出
---
+++ user/userInfo/userInfo.js
@@ -0,0 +1,42 @@
+var form = layui.form;
+
+$.ajax({
+    url:"/my/userinfo",
+    success:function(res){
+        layer.msg(res.message);
+        console.log(res);
+        if(res.status == 0) {
+            // var data = res.data;
+            // $("input[name = 'username']").val(data.username);
+            // $("input[name = 'nickname']").val(data.nickname);
+            // $("input[name = 'email']").val(data.email);
+            // 快速赋值，layUI
+            form.val("user",res.data);
+        }
+    }
+})
+
+
+// ---------------更新数据-----------
+// 1、初始化赋值id
+// 2、收集不会收集disabled
+$("form").on("submit", function(e){
+    e.preventDefault();
+
+    // 收集数据
+    var data = $(this).serialize();
+    // 发送请求
+    $.ajax({
+        type:"post",
+        url:"/my/userinfo",
+        data:data,
+        success:function(res){
+            layer.msg(res.message);
+            if(res.status == 0){
+                // 业务设计
+                // 
+               window.parent.get();
+            }
+        }
+    })
+})
