5bcd84a929fce18ec4317df152b6dd525cc291f5
lwq <15565680981@163.com>
2020-11-22 11:50:39 +0800

    l11 文章发布已完成

--- article/category/category.js
+++ article/category/category.js
@@ -53,25 +53,128 @@ $(".add").click(function () {
         title: '添加类别',
         content: add_str,
         area: ['500px', '250px'],
-        success:function(layero, index){
+        success: function (layero, index) {
             add_sub(index);
         }
     })
 })
 
 
-function add_sub(numb){
-    $("#add_form").on("submit", function(e){
+function add_sub(numb) {
+    $("#add_form").on("submit", function (e) {
         e.preventDefault();
         var data = $(this).serialize();
         $.ajax({
-            type:'POST',
-            url:'/my/article/addcates',
+            type: 'POST',
+            url: '/my/article/addcates',
             data: data,
-            success:function(res){
+            success: function (res) {
                 layer.msg(res.message);
-                if(res.status === 0){
+                if (res.status === 0) {
                     getList();
+                    layer.close(numb);
+                }
+            }
+        })
+    })
+}
+
+// --------------删除-----------------
+// /my/article/deletecate/:id  为了获取id值！
+// 1.事件委托
+$("tbody").on("click", ".delete", function(e) {
+    var mytoken = localStorage.getItem("token");
+    // 2.获取id；初始化的时候，需要把id和删除按钮绑定在一起；
+    var id = $(e.target).attr("myid");
+  
+    // 3.发送删除请求
+    $.ajax({
+      url: "/my/article/deletecate/"+ id,
+      success: function(res) {
+        // 4.成功后：重新加载列表
+        layer.msg(res.message);
+        if (res.status === 0) {
+            localStorage.setItem("token", mytoken);
+          // 删除成功，重新渲染
+          getList();
+        }
+      }
+    })
+  });
+  
+// -----------------编辑---------------
+
+var edit_str = `
+<form class="layui-form add-form" action="" style="margin: 30px; margin-left: 0px;" id="edit_form" lay-filter="edit">
+  <div class="layui-form-item">
+    <label class="layui-form-label">类别名称</label>
+    <div class="layui-input-block">
+      <input type="text" name="name" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+    </div>
+  </div>
+  <div class="layui-form-item">
+    <label class="layui-form-label">类别别名</label>
+    <div class="layui-input-block">
+      <input type="text" name="alias" required lay-verify="required" placeholder="请输入标题" autocomplete="off" class="layui-input">
+    </div>
+  </div>
+  <input type="hidden" name="Id">
+  <div class="layui-form-item">
+    <div class="layui-input-block">
+      <button class="layui-btn" lay-submit >确认修改</button>
+    </div>
+  </div>
+</form>`;
+
+// 1.事件委托
+var form = layui.form;
+$("tbody").on("click", ".edit", function(e){
+    var id = $(e.target).attr("myid");
+    $.ajax({
+        url:'/my/article/cates/' + id,
+        success:function(res){
+            if(res.status === 0){
+                layer.open({
+                    type: 1,
+                    title: '编辑类别',
+                    content: edit_str,
+                    area: ['500px', '250px'],
+                    // 层创建完毕时,里面有显示获取到数据
+                    success: function (layero, index) {
+                        // 5. layui.form
+                        //  5.1 去模板字符串里面新添加 type=hidden input;
+                        //  5.2 layui.form;  form表单上 lay-filter="edit"
+                        form.val("edit", res.data);
+
+                        // 6.注册提交事件
+                        edit_sub(index);
+                    }
+                })
+            }
+        }
+    })
+})
+
+
+// 6.给form注册提交事件
+function edit_sub(numb) {
+    $("#edit_form").on("submit", function (e) {
+        e.preventDefault();
+
+        // 6.1 获取数据
+        var data = $(this).serialize();
+
+        // 6.2 提交
+        $.ajax({
+            url: "/my/article/updatecate",
+            type: "post",
+            data: data,
+            success: function (res) {
+                layer.msg(res.message);
+                if (res.status === 0) {
+                    // 添加成功，重新渲染列表
+                    getList();
+                    // 关闭弹出层
                     layer.close(numb);
                 }
             }
---
+++ article/publisher/publisher.js
@@ -0,0 +1,119 @@
+// --------------------------------------------------加载分类数据
+var form = layui.form;
+$.ajax({
+  url: '/my/article/cates',
+  success: function(res) {
+
+    if (res.status == 0) {
+      var str = "";
+      $.each(res.data, function(index, ele) {
+        str += `<option value="${ele.Id}">${ele.name}</option>`;
+      });
+      $("select").html(str);
+
+      // form：更新渲染；
+      form.render('select');
+    }
+  }
+});
+
+
+// -------------------------------------------------初始化富文本
+initEditor();
+
+
+// -------------------------------------------------图片裁剪
+function initCropper() {
+  $('#image').cropper({
+    // 纵横比(宽高比)
+    aspectRatio: 400 / 280,
+    // 指定预览区域
+    preview: '.img-preview' // 指定预览区的类名（选择器）
+  });
+
+  // 选择图片
+  $(".select").click(function() {
+    $("#file").click();
+  });
+
+  // 弹窗：选择某个文件 file状态发生改变 change
+  $("#file").change(function() {
+
+    // 1.文件对象；
+    var obj = this.files[0];
+
+    // 2.URL内置对象 文件对象----> 创建临时地址
+    var src = URL.createObjectURL(obj);
+
+    // 3.替换: 插件进行替换
+    $('#image').cropper("replace", src);
+  });
+}
+initCropper();
+
+
+// ------------------------------------------------发布
+$("form").on("submit", function(e) {
+  e.preventDefault();
+
+  // 提交数据 formData(); ajax配置！后台接口支持！必须有name属性配置；
+  var fd = new FormData(this);
+  // console.log(fd);
+
+  // 遍历：可以看到内部已经添加什么数据？
+  // fd.forEach(function (val, key) {
+  //   console.log(val, key);
+  // })
+
+  // 
+  // 原因：这个两个区域，已经被各自插件所加载了！
+  // 解决：想办法把img 富文本内容 添加到fd；
+
+  //   1.富文本有名，没有值；需要重新设置下！查文档找如何获取内容！
+  fd.set("content", tinyMCE.activeEditor.getContent());
+  //   2.添加 裁剪后的图片信息   cover_img：值 文件对象；
+  //     借助 canvas 查文档 获取裁剪后的文件对象！
+  var canvas = $('#image').cropper('getCroppedCanvas', {
+    width: 400,
+    height: 280
+  });
+  //    canvas 用户获取文件对象,不是前面用户 base64字符串
+  canvas.toBlob(function(file) { // 文件对象
+    // 添加img 参数值：
+    fd.append("cover_img", file);
+
+    // 
+    // 完成添加
+    $.ajax({
+      type: 'POST',
+      url: '/my/article/add',
+      // fd:对象;
+      data: fd,
+      // 提交formdata数据，必须加下面两个选项
+      processData: false,
+      contentType: false,
+      success: function(res) {
+        layer.msg(res.message);
+        // 设计：
+        if (res.status == 0) {
+          // 页面转跳到 文章列表 页面
+          location.href = '/article/article/article.html';
+
+          // 响应dom节点 类名添加
+          var dom = window.parent.document.querySelector("#wzlist");
+          $(dom).addClass("layui-this").next().removeClass("layui-this");
+        }
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+});
